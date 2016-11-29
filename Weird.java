e82a8295744d33a489542c26da57e999d777cf3f
Shabz17 <shabnamsidhik17@gmail.com>
2016-11-29 06:40:45 +0530

    Execution flow and Java8 Interfaces

--- Weird.java
+++ Weird.java
@@ -58,6 +58,77 @@ class R extends Q{
 
 class S extends R{
 	{
+		
+		System.out.println("Instance initializer block in CLASS S ");
+	}
+	S(){
+		System.out.println("Constructor in S");
+	 }
+	 static{
+		 System.out.println("static block in S");
+	 }
+		static{
+			System.out.println("second static block in S");
+		}
+}
+
+
+interface Base{
+	default int getN(){
+		return 5;
+	}
+	
+	default int getT(){
+		return 35;
+	}
+	
+	static int getH(){
+		return 34;
+	}
+
+}
+
+interface Child extends Base{
+	
+//	static int getN(){ //This static method cannot hide the instance method from Base
+//		return 15;
+//	}
+	
+	 int getN(); // is possible to override default getN() of Base using abstract getN() in child
+	
+	 default int getT(){  // overrides
+			return 39;
+		}
+	 
+	default int getH(){ // default methods CAN OVERRIDE STATIC METHOD in base interface
+			return 34;
+		}
+	 
+}
+
+
+
+public class Weird {
+   public static void main(String[] args) {
+	   R s = new R();// static q,r,s.. II,Cons (Q); II,Cons (R); II,Cons (S),
+	   
+	   System.out.println("=========================");
+	   S ss = new S();
+	   System.out.println("NOTE THAT STATIC BLOCKS ARE EXECUTED ONLY ONCE UNLIKE INSTANCE INITIALIZER AND CONSTRUCTOR");
+}
+}
+
+	 
+//	static void getName(){
+//		   
+//	   }
+	 
+//	 Trying to override a static method with a non-static method (and vice-versa) in a class will result in a compilation error. 
+//   Even in case of interfaces, a subinterface not override a default method with a static method. 
+}
+
+class S extends R{
+	{
 		System.out.println("Instance initializer block in CLASS S ");
 	}
 	S(){
